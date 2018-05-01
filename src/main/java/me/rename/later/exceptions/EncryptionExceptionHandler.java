f20d8ca900acddb15eba1f1b3d527af63c1e568d
Charlie Curtis <charlie-curtis@outlook.com>
2018-05-01 00:16:02 -0400

    remove unused imports

--- src/main/java/me/rename/later/exceptions/EncryptionExceptionHandler.java
+++ src/main/java/me/rename/later/exceptions/EncryptionExceptionHandler.java
@@ -2,20 +2,10 @@ package me.rename.later.exceptions;
 
 import org.apache.commons.lang3.ArrayUtils;
 
-import javax.crypto.NoSuchPaddingException;
-import javax.ws.rs.core.Response;
-import java.security.GeneralSecurityException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-
 public class EncryptionExceptionHandler extends Exception {
 
     //exceptions that are caused by bad input from the client
     private static Class[] clientExceptionClasses = {
-        //NoSuchAlgorithmException.class,
-        //NoSuchPaddingException.class,
-        //InvalidAlgorithmParameterException.class
         java.security.InvalidKeyException.class
     };
     public static boolean isClientError(Exception ex)
